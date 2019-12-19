0bcd280790bfb49e408e3c8e572e179cb98165de
Lee Bardon <leerbardon@gmail.com>
2019-12-19 14:56:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,4 @@
-def find_element_index(array, value_to_find)
-  array.length.times do |i|
-    if array[i] == value_to_find
-      return i
-    end
-    nil
-  end
-end
+
 
 
 def find_max_value(array)
