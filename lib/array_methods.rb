d12b74a260f87f87b13161dd9ae64937f041ec7a
Lee Bardon <leerbardon@gmail.com>
2019-12-19 14:55:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |i|
-    if array[i] == value_to_find
+  array.length.times do |count|
+    if array[count] == value_to_find
       return i
     end
     nil
